6f769f443de78e2f8d684bb1c1e2869dba66ab20
mrowa223 <88025828+mrowa223@users.noreply.github.com>
2022-08-12 13:23:16 +0600

    Add files via upload

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { SearchContext } from "../../App";
+
+import styles from "./Search.module.scss";
+export const Search = () => {
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g data-name="Layer 2" id="Layer_2">
+          <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
+        </g>
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {searchValue && (
+        <svg onClick={() => setSearchValue("")} className={styles.clearIcon}>
+          <g>
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
+          </g>
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { render } from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//  to get only store without export default
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
+import { BrowserRouter } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
